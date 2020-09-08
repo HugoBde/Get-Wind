d2b167b7901be013df2c17bcafd3d6e4454defe7
Hugo Bouderlique <bouderliquehugo@gmail.com>
2020-09-08 20:31:15 +1000

    Updated the architecture of program in order to save used space on drive.

--- get_Wind.py
+++ get_Wind.py
@@ -1,36 +1,35 @@
 import requests
-import sys
 import os
 from time import sleep
 #from bs4 import BeautifulSoup
 
-def clearLastLine():
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-
 def getHtml(webUrl):
-    print('Accessing ' + webUrl + '...')
     htmlContent = requests.get(webUrl)
-    #clearLastLine()
     if htmlContent.status_code == 200:
-        print("URL reached successfully.")
         return htmlContent.text
     else:
         print('Error '+ str(htmlContent.status_code) +' ' + htmlContent.reason)
         return None
 
 
-def saveHtml(webUrl, webPage):
-    print('Saving web page ...')
-    with open('Saved_Pages/'+webUrl.split('/')[-1]+'.html', 'w') as myPage:
-        myPage.write(webPage)
-    #clearLastLine()
-    print('Page Saved.')
+def saveHtml(webUrl):
+    print('Downloading ' + webUrl + ' ...')
+    filePath = webUrl.split('/')[-1]+'.html'
+    with open(filePath, 'w') as myPage:
+        try:
+            myPage.write(getHtml(webUrl))
+            print('Page Downloaded.')
+        except:
+            print(webUrl + ' was not downloaded.')
+    
 
+with open('url_List.txt' , 'r') as urlList:
+    for url in urlList:
+        url = url.replace("\n","")
+        saveHtml(url)
+print('Content Downloaded.')
 
-if not os.path.exists('Saved_Pages/'):
-    os.mkdir('Saved_Pages/')
-urlList = open('url_List.txt' , 'r')
-for url in urlList:
-    url = url.replace("\n","")
-    saveHtml(url,getHtml(url))
+for htmlFile in os.listdir():
+    if htmlFile.split('.')[-1] == 'html':
+        os.remove(htmlFile)
+#input('Continue ...')
