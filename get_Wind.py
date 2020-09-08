7ee9e1b644db489b00d39c4f5bffd65a80c33ed9
Hugo Bouderlique <bouderliquehugo@gmail.com>
2020-09-08 17:15:36 +1000

    Progress

--- get_Wind.py
+++ get_Wind.py
@@ -8,11 +8,11 @@ def clearLastLine():
     sys.stdout.write('\x1b[2K')
 
 def getHtml(webUrl):
-    print('Accessing URL...')
+    print('Accessing ' + webUrl + '...')
     htmlContent = requests.get(webUrl)
-    clearLastLine()
+    #clearLastLine()
     if htmlContent.status_code == 200:
-        print("URL reached successfully.")
+        print("URL reached successfully. \n")
         return htmlContent.text
     else:
         print('Error '+ str(htmlContent.status_code) +' ' + htmlContent.reason)
@@ -21,11 +21,13 @@ def getHtml(webUrl):
 
 def saveHtml(webUrl, webPage):
     print('Saving web page ...')
-    with open(webUrl.split('/')[-1]+'.html', 'w') as myPage:
+    with open('Saved Pages/'+webUrl.split('/')[-1]+'.html', 'w') as myPage:
         myPage.write(webPage)
     sleep(2)
-    clearLastLine()
-    print('Page Saved.')
+    #clearLastLine()
+    print('Page Saved.\n')
 
-url = 'http://www.meteo.nc/nouvelle-caledonie/observations/dernieres-24h'
-saveHtml(url,getHtml(url))
+urlList = open('url_List.txt' , 'r')
+for url in urlList:
+    url = url.replace("\n","")
+    saveHtml(url,getHtml(url))
