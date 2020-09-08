7a7bb427430b79582c08b1fd29dcf7291a93d754
Hugo Bouderlique <bouderliquehugo@gmail.com>
2020-09-08 21:04:24 +1000

    Started implementing parser in code

--- get_Wind.py
+++ get_Wind.py
@@ -1,18 +1,18 @@
-import requests
+import requests                 #Importing all necessary libraries
 import os
 from time import sleep
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-def getHtml(webUrl):
+def getHtml(webUrl):            #Download the html code of a page hosted at webUrl
     htmlContent = requests.get(webUrl)
-    if htmlContent.status_code == 200:
+    if htmlContent.status_code == 200:  #If the GET request is successful, page is downloaded
         return htmlContent.text
-    else:
+    else:                               #Otherwise the error message is returned to the user
         print('Error '+ str(htmlContent.status_code) +' ' + htmlContent.reason)
         return None
 
 
-def saveHtml(webUrl):
+def saveHtml(webUrl):           #The html code is then stored into a file in the program directory
     print('Downloading ' + webUrl + ' ...')
     filePath = webUrl.split('/')[-1]+'.html'
     with open(filePath, 'w') as myPage:
@@ -29,7 +29,16 @@ with open('url_List.txt' , 'r') as urlList:
         saveHtml(url)
 print('Content Downloaded.')
 
+
+
 for htmlFile in os.listdir():
     if htmlFile.split('.')[-1] == 'html':
-        os.remove(htmlFile)
+        with open(htmlFile) as myFile:
+            mySoup = BeautifulSoup(myFile, 'lxml')
+            for cell in mySoup.td:
+                if 'obs' in cell['class']:
+                    print (cell.get_text())
+            
+        #os.remove(htmlFile)
+
 #input('Continue ...')
